31b0b9c52a221c22b48079af2f83baf1ad916475
varushvarsha <varush.varsha777@gmail.com>
2025-05-19 11:27:03 +0530

    added

---
+++ mainwindow.cpp
@@ -0,0 +1,61 @@
+
+
+
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QLayout>
+#include <QPushButton>
+#include <QTextBrowser>
+#include <QLineEdit>
+#include <QObject>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QWidget *centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+    setWindowTitle("Sentimeter");
+
+    //! Central Layout
+    QVBoxLayout *mainLayout = new QVBoxLayout();
+    centralWidget->setLayout(mainLayout);
+
+    //! Auxiliary Layouts
+    QHBoxLayout  *upperLayout = new QHBoxLayout();
+    QHBoxLayout *middleLayout = new QHBoxLayout();
+    QHBoxLayout  *lowerLayout = new QHBoxLayout();
+
+    mainLayout->addLayout(upperLayout);
+    mainLayout->addLayout(middleLayout);
+    mainLayout->addLayout(lowerLayout);
+
+
+    //! upperLayout
+    QLineEdit *searchBar = new QLineEdit();
+    searchBar->setPlaceholderText("Search");
+    upperLayout->addWidget(searchBar);
+
+    QPushButton *buttonClose = new QPushButton("Close");
+    QPushButton *buttonSearch = new QPushButton("Search");
+    upperLayout->addWidget(buttonSearch);
+    upperLayout->addWidget(buttonClose);
+
+    //! middleLayout
+    //! TODO
+
+    //! lowerLayout
+    QTextBrowser *currentStatus = new QTextBrowser();
+    lowerLayout->addWidget(currentStatus);
+
+    QObject::connect(buttonClose, &QPushButton::clicked, this, &MainWindow::close);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
